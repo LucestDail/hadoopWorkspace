61741bef2af4944fba4364d00a65c2dbc0507329
hyun <dhtmdgussksm@naver.com>
2021-01-28 17:30:36 +0900

    update
    test code about delay departure, delay arrival separated by unique carrier code

---
+++ hadoopweb/src/main/java/dataexpo/servlet/CarrMultiServlet.java
@@ -0,0 +1,108 @@
+package dataexpo.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileAlreadyExistsException;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import dataexpo.mapper.CarrMultiDelayMapper;
+import dataexpo.mapper.MultiDelayMapper;
+import dataexpo.reducer.CarrMultiDelayReducer;
+import dataexpo.reducer.MultiDelayReducer;
+
+public class CarrMultiServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public CarrMultiServlet() {
+        super();
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		String year = request.getParameter("year");
+		String input = "C:/Users/dhtmd/myworkspace/hadoopWorkspace/hadoopweb/infile/" + year + ".csv";
+		String output = request.getSession().getServletContext().getRealPath("/") + "output/carrdelay/" + year;
+		Configuration conf = new Configuration();
+		try {
+			System.out.println("into try");
+			
+			Job job = new Job(conf,"CarrMultiServlet");
+			
+			FileInputFormat.addInputPath(job, new Path(input));
+			FileOutputFormat.setOutputPath(job, new Path(output));
+			
+			job.setJarByClass(this.getClass());
+			job.setMapperClass(CarrMultiDelayMapper.class);
+			job.setReducerClass(CarrMultiDelayReducer.class);
+			
+			job.setInputFormatClass(TextInputFormat.class);
+			job.setOutputFormatClass(TextOutputFormat.class);
+			
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(IntWritable.class);
+			
+			MultipleOutputs.addNamedOutput(job, "departure", TextOutputFormat.class, Text.class, IntWritable.class);
+			MultipleOutputs.addNamedOutput(job, "arrival", TextOutputFormat.class, Text.class, IntWritable.class);
+			
+			job.waitForCompletion(true);
+			
+			System.out.println("try end");
+			
+		}catch(FileAlreadyExistsException e) {
+			System.out.println("기존 파일 존재 : " + output);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		String arrivalFile = "arrival-r-00000";
+		String departureFile = "departure-r-00000";
+		request.setAttribute("file", year);
+		Path outDeparture = new Path(output + "/" + departureFile);
+		Path outArrival = new Path(output + "/" + arrivalFile);
+		FileSystem fs = FileSystem.get(conf);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(outDeparture)));
+		Map<String, Integer> map1 = new TreeMap<String, Integer>();
+		String line = null;
+		while((line = br.readLine()) != null) {
+			String[] v = line.split("\t");
+			int cnt = Integer.parseInt(v[1].trim());
+			map1.put(v[0].trim(), cnt);
+		}
+		request.setAttribute("map1", map1);
+		
+		br = new BufferedReader(new InputStreamReader(fs.open(outArrival)));
+		Map<String, Integer> map2 = new TreeMap<String, Integer>();
+		line = null;
+		while((line = br.readLine()) != null) {
+			String[] v = line.split("\t");
+			int cnt = Integer.parseInt(v[1].trim());
+			map2.put(v[0].trim(), cnt);
+		}
+		request.setAttribute("map2", map2);
+		String jsp = "/dataexpo/dataexpo4.jsp";
+		RequestDispatcher v = request.getRequestDispatcher(jsp);
+		v.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ hadoopweb/src/main/webapp/dataexpo/dataexpo4.jsp
@@ -0,0 +1,60 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="core" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>항공사별 출발 도착 지연 데이터 분석</title>
+<script type="text/javascript"
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script type="text/javascript" 
+  src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+</head>
+<body>
+<h3>항공사별 출발 / 도착 지연 분석</h3>
+<form action="${pageContext.request.contextPath}/CarrMultiServlet" method="post" name="f">
+	<select name="year">
+		<core:forEach var="y" begin="1987" end="1988">
+			<option <core:if test="${param.year==y}"> selected</core:if>>${y}</option>
+		</core:forEach>
+	</select>
+	<input type="submit" value="분석">
+</form>
+<core:if test = "${!empty file}">
+	<div id = "canvas-holder" style="width:50%; height:300px;">
+		<canvas id="chart" width="100%" height="100%"></canvas>
+	</div>
+	<script type="text/javascript">
+	var randomColorFactor = function(){
+		return Math.round(Math.random() * 255);
+	}
+	var randomColor = function(opacity){
+		return "rgba("+randomColorFactor() + "," + randomColorFactor() + "," + randomColorFactor() + "," + (opacity||".3") + ")";
+	}
+	rcolor = randomColor(1);
+	rcolor2 = randomColor(0.5);
+	var config = {
+			type : "bar",
+			data : {
+				datasets : [{
+					label : "${file}년 출발 지연 건수",
+					data : [<core:forEach items="${map1}" var="m">"${m.value}",</core:forEach>],
+					backgroundColor : [<core:forEach items="${map1}" var="m">rcolor,</core:forEach>]
+				},{
+					label : "${file}년 도착 지연 건수",
+					data : [<core:forEach items="${map2}" var="m">"${m.value}",</core:forEach>],
+					backgroundColor : [<core:forEach items="${map2}" var="m">rcolor2,</core:forEach>]
+				}],
+				labels : [<core:forEach items="${map1}" var="m">"${m.key}",</core:forEach>]
+			},
+			options : {responsive : true}
+	};
+	window.onload = function(){
+		var ctx = document.getElementById("chart").getContext("2d");
+		new Chart(ctx,config);
+	}
+	</script>
+</core:if>
+</body>
+</html>
